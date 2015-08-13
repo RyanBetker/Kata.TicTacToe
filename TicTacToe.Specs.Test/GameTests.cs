38e2b7b853dea6ba1660f98153214abb152372eb
wordracr <technologyryan@outlook.com>
2015-08-13 14:13:24 -0500

    Initial commit with 7 tests (playTurn and slight validation logic)

---
+++ TicTacToe.Specs.Test/GameTests.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TicTacToe.Specs.Test
+{
+    [TestClass]
+    public class GameTests
+    {
+        [TestMethod]
+        public void GameStoresPositionTopLeft()
+        {
+            var g = new Game();
+            var playerType = PlayerType.X;
+
+            g.PlayTurn(playerType, 0, 0);
+
+            Assert.AreEqual(playerType, g.Board[0, 0]);
+        }
+
+        [TestMethod]
+        public void GameStoresPositionTopMiddle()
+        {
+            var g = new Game();
+            var playerType = PlayerType.X;
+
+            g.PlayTurn(playerType, 0, 1);
+
+            Assert.AreEqual(playerType, g.Board[0, 1]);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GameDoesNotAllowRowUnder0()
+        {
+            var g = new Game();
+            var playerType = PlayerType.X;
+
+            g.PlayTurn(playerType, -1, 1);
+
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GameDoesNotAllowColumnUnder0()
+        {
+            var g = new Game();
+            var playerType = PlayerType.X;
+
+            g.PlayTurn(playerType, -1, 0);
+
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GameDoesNotAllowRowOver2()
+        {
+            var g = new Game();
+            var playerType = PlayerType.X;
+
+            g.PlayTurn(playerType, 3, 1);
+
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GameDoesNotAllowColumnOver2()
+        {
+            var g = new Game();
+            var playerType = PlayerType.X;
+
+            g.PlayTurn(playerType, 2, 3);
+
+        }
+
+        [TestMethod]
+        public void GameStoresAccuratePlayer()
+        {
+            var g = new Game();
+            var playerType = PlayerType.O;
+
+            g.PlayTurn(playerType, 0, 0);
+
+            Assert.AreEqual(playerType, g.Board[0, 0]);
+        }
+    }
+}
---
+++ TicTacToe.Specs/Game.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Specs
+{
+    public enum PlayerType
+    {
+        X, O
+    }
+
+    public class Game
+    {
+        public Game()
+        {
+            Board = new PlayerType?[3, 3];
+        }
+
+        /// <summary>
+        /// Plays a turn
+        /// </summary>
+        /// <param name="playerType">Player type</param>
+        /// <param name="row">Row in which to play. Use 0-2</param>
+        /// <param name="column">Column in which to play. Use 0-2</param>
+        public void PlayTurn(PlayerType playerType, int row, int column)
+        {
+            if (row > 2 || row < 0)
+            {
+                throw new ArgumentException("Row value must be between 0 and 2");
+            }
+
+            if (column > 2 || column < 0)
+            {
+                throw new ArgumentException("Column value must be between 0 and 2");
+            }
+
+            Board[row, column] = playerType;
+            return;
+        }
+
+        /// <summary>
+        /// First position is for rows, second is columns
+        /// </summary>
+        public PlayerType?[,] Board { get; private set; }
+    }
+}
